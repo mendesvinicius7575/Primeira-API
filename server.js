496e8aab4f9c907df9f0ad4b6aca356fe265bc6b
Vinicius Emanoel Mendes <108685945+mendesvinicius7575@users.noreply.github.com>
2022-08-04 20:38:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-//Chamando o Cors para criar conexao com o Front
-const cors = require('cors');
-
-//Chamando Puppeteer
+const express = require('express');
 const puppeteer = require('puppeteer')
 
-//Iniciando o express
-const express = require('express');
 const server = express();
 
-//Usar o Cors, qualquer server pode acessar minha aplicação
-server.use(cors())
+server.get('/', async (request, response) => {
 
-server.get('/', async (req,res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://countrymeters.info/pt/Brazil');
+    await page.goto('https://countrymeters.info/pt/Brazil')
 
-    const pageContent = await page.evaluate(() => {
-        return{
-            info: document.querySelector('#cp1').innerHTML
+  
+
+    const conteudoPage = await page.evaluate(() => {
+        return {
+            subtitle: document.querySelector('#cp1').innerHTML
         }
-    });
+    })
+    console.log('pageContent:', conteudoPage)
 
-   
+    await browser.close();
 
-    res.send(
-            pageContent.info    
-    );
-    console.log('Conectou tudo ok')
-    
+    let num = conteudoPage.subtitle.replace(/ /g,"")
+
+
+    response.send({
+        "A população Brasileira:": Number(num),
+        "A População Brasileira Dividia por 2 é": num / 2,
+        "Dado puxado em tempo real do Site" : "https://countrymeters.info/pt/Brazil"
+    })
+});
+
+
+server.listen(3000, () => {
+    console.log(`Subiu com sucesso. Acesse http://localhost:3000`);
 })
-
-server.listen('5000')
