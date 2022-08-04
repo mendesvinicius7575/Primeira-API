9bb141165ee9b3f448995053f1db602708cdbc7e
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-04 20:28:00 -0300

    Testes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 //Chamando o Cors para criar conexao com o Front
 const cors = require('cors');
 
+//Chamando Puppeteer
+const puppeteer = require('puppeteer')
+
 //Iniciando o express
 const express = require('express');
 const server = express();
@@ -8,13 +11,24 @@ const server = express();
 //Usar o Cors, qualquer server pode acessar minha aplicação
 server.use(cors())
 
-server.get('/', (req,res) => {
+server.get('/', async (req,res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://countrymeters.info/pt/Brazil');
+
+    const pageContent = await page.evaluate(() => {
+        return{
+            info: document.querySelector('#cp1').innerHTML
+        }
+    });
+
+   
+
+    res.send(
+            pageContent.info    
+    );
     console.log('Conectou tudo ok')
-    return res.json([
-        {name: 'Vinicius'},
-        {name: 'André'},
-        {name: 'Roberval'},
-    ])
+    
 })
 
 server.listen('5000')
