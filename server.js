7cdddd7babc8401c6cdeb0980c54ea040807024c
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-05 09:47:48 -0300

    Mais Dados
    Coletei mais dados mas dessa vez de duas paginas diferentes. Dessa forma posso trabalhar com eles em conjunto

--- server.js
+++ server.js
@@ -9,25 +9,56 @@ server.get('/', async (request, response) => {
     const page = await browser.newPage();
     await page.goto('https://countrymeters.info/pt/Brazil')
 
-  
+    const browser2 = await puppeteer.launch();
+    const page2 = await browser2.newPage();
+    await page2.goto('https://countrymeters.info/pt/Argentina')
 
+
+  
+    //page brasil
     const conteudoPage = await page.evaluate(() => {
         return {
-            subtitle: document.querySelector('#cp1').innerHTML
+            brasil: document.querySelector('#cp1').innerHTML,
+            mortes: document.querySelector('#cp9').innerHTML
         }
     })
-    console.log('pageContent:', conteudoPage)
+    //page argentina
+    const conteudoPage2 = await page2.evaluate(() => {
+        return {
+            arg: document.querySelector('#cp1').innerHTML,
+            mortes: document.querySelector('#cp9').innerHTML
+        }
+    })
+
 
     await browser.close();
 
-    let num = conteudoPage.subtitle.replace(/ /g,"")
+    //pegando dados para trabalhar
+    let numB = conteudoPage.brasil.replace(/ /g,"")
+    let numMortes = conteudoPage.mortes.replace(/ /g,"")
+    let numArg = conteudoPage2.arg.replace(/ /g,"")
+    let numMortesArg = conteudoPage2.mortes.replace(/ /g,"")
 
+    //Criei um objeto
+    const obj = [{
+        "A população Brasileira:": Number(numB),
+        "A População Brasileira Dividia por 2 é": numB / 2,
+        "Pessoas morreram hoje:": Number(numMortes),
+        "Dados puxado do Site" : "https://countrymeters.info/pt/Brazil"},
+        {"A população Argentina:": Number(numArg),
+        "A População Argentina Dividia por 2 é": numB / 2,
+        "Pessoas morreram hoje:": Number(numMortesArg),
+        "Dados puxado do Site" : "https://countrymeters.info/pt/Argentina"
+        }
+    ]
 
-    response.send({
-        "A população Brasileira:": Number(num),
-        "A População Brasileira Dividia por 2 é": num / 2,
-        "Dado puxado em tempo real do Site" : "https://countrymeters.info/pt/Brazil"
-    })
+    let data = JSON.stringify(obj)
+    
+
+    response.send(
+        obj
+        
+        )
 });
 
 
